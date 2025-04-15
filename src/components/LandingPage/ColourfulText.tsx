ade33eb6cfb8d4fa1bb873ae9481f804e67ee415
Dunsin <ayodejiabisuwa23@gmail.com>
2025-04-15 06:21:56 +0100

    creaetd pages

--- src/components/LandingPage/ColourfulText.tsx
+++ src/components/LandingPage/ColourfulText.tsx
@@ -2,14 +2,16 @@
 import React from "react";
 import ColourfulText from "@/components/ui/colourful-text";
 import { motion } from "motion/react";
+import { useRouter } from "next/navigation";
 
 export default function ColourfulTextDemo() {
+    const router = useRouter()
   return (
     <div className="h-screen  w-full flex flex-col space-y-5 items-center justify-center relative overflow-hidden bg-black">
       <motion.img
         src="grid.jpg"
         className="h-full w-full  object-cover absolute inset-0 [mask-image:radial-gradient(circle,transparent,black_80%)] pointer-events-none"
-        initial={{ opacity: 0 }}
+        initial={{ opacity: 0 }}        
         animate={{ opacity: 0.5 }}
         transition={{ duration: 1 }}
       />
@@ -18,7 +20,7 @@ export default function ColourfulTextDemo() {
         <ColourfulText text="peer-to-peer" /> <br />
         energy trading.
       </h1>
-      <button className="">Get Started</button>
+      <button className="" onClick={() => router.push("/wallet")}>Get Started</button>
     </div>
   );
 }
