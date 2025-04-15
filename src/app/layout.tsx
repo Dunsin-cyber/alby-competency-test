61439182549d02dcaeaffbb772a3c62c5d491b9f
Dunsin <ayodejiabisuwa23@gmail.com>
2025-04-15 13:30:23 +0100

    fix : multiple attribute with the same name in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,6 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
         <link
-          rel="preconnect"
           href="https://fonts.googleapis.com/css2?family=Winky+Rough:ital,wght@0,300..900;1,300..900&display=swap"
           rel="stylesheet"
         />
